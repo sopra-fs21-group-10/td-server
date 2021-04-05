6faa597c937cb345ff9dbfe72fb04994268d9e2c
Thahit <nicolas.blumer@gmail.com>
2021-04-05 14:26:43 +0200

    fixed failing test
    Signed-off-by: Thahit <you@example.comnicolas.blumer@gmail.com>

--- src/test/java/ch/uzh/ifi/hase/soprafs21/controller/UserControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs21/controller/UserControllerTest.java
@@ -3,7 +3,6 @@ package ch.uzh.ifi.hase.soprafs21.controller;
 import ch.uzh.ifi.hase.soprafs21.constant.UserStatus;
 import ch.uzh.ifi.hase.soprafs21.entity.User;
 import ch.uzh.ifi.hase.soprafs21.repository.UserRepository;
-import ch.uzh.ifi.hase.soprafs21.rest.dto.UserPostDTO;
 import ch.uzh.ifi.hase.soprafs21.rest.dto.UserPostInDTO;
 import ch.uzh.ifi.hase.soprafs21.service.UserService;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -19,7 +18,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.net.URI;
 import java.util.Collections;
 import java.util.List;
 
@@ -28,7 +26,8 @@ import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * UserControllerTest
@@ -52,7 +51,7 @@ public class UserControllerTest {
         // given
         User user = new User();
         user.setUsername("firstname@lastname");
-        user.setStatus(UserStatus.OFFLINE);
+        user.setStatus(UserStatus.ONLINE);
 
         List<User> allUsers = Collections.singletonList(user);
 

40fd0d5537683c6185da37a5dfdf87fa6b0388b6
maurohirt <45426145+maurohirt@users.noreply.github.com>
2021-04-18 12:54:15 +0200

    Enum for LobbyStatus of Player2 & LobbyPostDTO crreated

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/rest/dto/LobbyPostDTO.java
@@ -0,0 +1,9 @@
+package ch.uzh.ifi.hase.soprafs21.rest.dto;
+
+public class LobbyPostDTO {
+    private Long id;
+
+    public Long getId() {return id;}
+
+    public void setId(Long id) {this.id = id;}
+}
--- src/main/java/ch/uzh/ifi/hase/soprafs21/rest/mapper/DTOMapper.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/rest/mapper/DTOMapper.java
@@ -1,6 +1,7 @@
 package ch.uzh.ifi.hase.soprafs21.rest.mapper;
 
 import ch.uzh.ifi.hase.soprafs21.entity.User;
+import ch.uzh.ifi.hase.soprafs21.rest.dto.LobbyPostDTO;
 import ch.uzh.ifi.hase.soprafs21.rest.dto.UserGetDTO;
 import ch.uzh.ifi.hase.soprafs21.rest.dto.UserPostDTO;
 import ch.uzh.ifi.hase.soprafs21.rest.dto.UserPostInDTO;
@@ -34,4 +35,7 @@ public interface DTOMapper {
     @Mapping(source = "username", target = "username")
     @Mapping(source = "status", target = "status")
     UserGetDTO convertEntityToUserGetDTO(User user);
+
+    @Mapping(source = "id", target = "id")
+    LobbyPostDTO convertEntitytoLobbyPostDTO(Long id);
 }

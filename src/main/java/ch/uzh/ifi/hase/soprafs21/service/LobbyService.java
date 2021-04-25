21a1a092fe1e6f18e5424f8e4868ccd6f7b6b9ab
maurohirt <45426145+maurohirt@users.noreply.github.com>
2021-04-26 01:24:51 +0200

    Update LobbyService.java

--- src/main/java/ch/uzh/ifi/hase/soprafs21/service/LobbyService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/service/LobbyService.java
@@ -58,6 +58,7 @@ public class LobbyService {
     }
     public Lobby addUserToLobby(Long lobbyId,User userToBeAdded){
         Lobby lobby = findLobbyById(lobbyId);
+      
         if(lobby.getPlayer2()!=null){
             throw new ResponseStatusException(HttpStatus.FORBIDDEN,"The lobby is full, a new player can not join");
         }
@@ -73,7 +74,7 @@ public class LobbyService {
             lobby.setLobbyStatus(PlayerLobbyStatus.WAITING);
             lobbyRepository.saveAndFlush(lobby);
         }
-        else {
+        else if (lobby.getOwner()==userToBeRemoved){
         lobbyRepository.delete(lobby);
         lobbyRepository.flush();
         }

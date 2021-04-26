9d19fb38652d3f0d7cc84afec62edf62ff594e0c
bzns <matej.gurica@uzh.ch>
2021-04-26 13:32:32 +0200

    Fixed mapping for UserPostDTO.

--- src/main/java/ch/uzh/ifi/hase/soprafs21/rest/dto/UserPostDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/rest/dto/UserPostDTO.java
@@ -2,6 +2,7 @@ package ch.uzh.ifi.hase.soprafs21.rest.dto;
 
 public class UserPostDTO {
     private String token;
+    private long id;
 
     public String getToken() {
         return token;
@@ -10,4 +11,8 @@ public class UserPostDTO {
     public void setToken(String token) {
         this.token = token;
     }
+
+    public long getId() { return id;}
+
+    public void setId(long id) {this.id = id;}
 }
